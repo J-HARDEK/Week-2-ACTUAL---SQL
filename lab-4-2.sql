d5fb97aaa702526da53b7e0008e156f6b9c4ae7b
John Hardek <john.hardek@kellogg.northwestern.edu>
2025-01-20 20:17:06 +0000

    practiced lab 4, reviewed with solutions

--- lab-4-2.sql
+++ lab-4-2.sql
@@ -5,6 +5,17 @@
 -- WHERE column1 = expression1
 -- AND column2 = expression2
 
+-- CODE WE WROTE:
+
+SELECT players.first_name, players.last_name
+FROM stats 
+  INNER JOIN teams ON teams.id = stats.team_id
+  INNER JOIN players ON players.id = stats.player_id
+WHERE teams.year = 2020
+AND teams.name = "Chicago Cubs";
+
+-- END
+
 -- Expected result: 47 rows starting with
 --
 -- +------------+-----------+
--- lab-4-3.sql
+++ lab-4-3.sql
@@ -2,6 +2,14 @@
 
 -- Expected result:
 --
+--CODE I WROTE:
+SELECT teams.name, players.first_name, players.last_name, MAX(home_runs)
+FROM stats 
+  INNER JOIN teams ON teams.id = stats.team_id
+  INNER JOIN players ON players.id = stats.player_id
+WHERE year = 2019;
+--END
+
 -- +---------------+------------+-----------+-----------+
 -- | New York Mets | Pete       | Alonso    | 53        |
 -- +---------------+------------+-----------+-----------+
